d2733fca854bdf04423bef28afe7ea0960121265
Laura <Laura@DESKTOP-BESSB6E>
2023-08-28 03:05:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Guess The Door");
+Console.WriteLine("  __________ ");
+Console.WriteLine(" |  __  __  |");
+Console.WriteLine(" | |  ||  | |");
+Console.WriteLine(" | |  ||  | |");
+Console.WriteLine(" | |__||__| |");
+Console.WriteLine(" |  __  __()|");
+Console.WriteLine(" | |  ||  | |");
+Console.WriteLine(" | |  ||  | |");
+Console.WriteLine(" | |__||__| |");
+Console.WriteLine(" |__________|");
+Console.WriteLine("             ");
+
+Console.WriteLine("Choose the number:\n 1     2     3");
+
+string userChoice = Console.ReadLine();
+Random random = new Random();
+int numberToGuess = random.Next(4);
+
+if (userChoice == numberToGuess.ToString())
+{
+    Console.WriteLine("Your choice is correct!");
+    userChoice = Console.ReadLine();
+    numberToGuess = random.Next(4);
+
+    if (userChoice == numberToGuess.ToString())
+    {
+        Console.WriteLine("Your choice is correct!");
+        userChoice = Console.ReadLine();
+
+        numberToGuess = random.Next(4);
+        if (userChoice == numberToGuess.ToString())
+        {
+            Console.WriteLine("VICTORY!");
+        }
+        else
+        {
+            Console.WriteLine("GAME OVER");
+            Console.WriteLine("Better luck next time");
+        }
+    }
+    else
+    {
+        Console.WriteLine("GAME OVER");
+    }
+}
+else
+{
+    Console.WriteLine("GAME OVER");
+}
+
+
